f6aa1bb092314be52daae38500dbf31243435569
liugenyuan <liugenyuan@zhihuijianshe.com>
2020-07-21 11:12:12 +0800

    new commit

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import App from './App.vue'
 
 import TestComponent from 'test-component'
 import './style/theme/element/theme/index.css'
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 
 Vue.use(TestComponent)
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
